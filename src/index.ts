883207e69e8403a07c5d6593cd7d9c0cac10cda8
Shulgin Ruslan <shulgin.ruslan@wb.ru>
2023-10-22 17:24:09 +0300

    some changes

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,5 @@ import { userService } from "./services/user.service";
 new Promise(res => res(userService.init()))
 .then(() => new Router().route())
 .then(() => cartService.init())
-.then(() => setTimeout(() => {
-  document.body.classList.add("is__ready");
-}, 250))
+.then(() => document.body.classList.add("is__ready"))
 .catch(e => console.log(e))
